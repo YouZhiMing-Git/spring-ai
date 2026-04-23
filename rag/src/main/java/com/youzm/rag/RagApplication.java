be7072a62430c03910744d822456690712ea9d49
youzm <youzhiming>
2026-04-23 17:32:36 +0800

    增加mcp

---
+++ rag/src/main/java/com/youzm/rag/RagApplication.java
@@ -0,0 +1,18 @@
+package com.youzm.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:youzhiming
+ * @date: 2026/4/23
+ * @description:
+ */
+
+@SpringBootApplication
+public class RagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagApplication.class, args);
+    }
+}
--- src/main/java/com/youzm/chatModel/loadBalancer/LoadBalancerStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.youzm.chatModel.loadBalancer;
-
-import org.springframework.ai.chat.model.ChatModel;
-
-import java.util.List;
-
-public interface LoadBalancerStrategy {
-    ChatModel select(List<ModelInstance> instances);
-}
